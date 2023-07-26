3258a1512db55dd999551a1b0f21ef3f80770ee0
57one <784537055@qq.com>
2023-07-26 16:54:09 +0800

    add global file

---
+++ global.cpp
@@ -0,0 +1,10 @@
+#include "global.h"
+
+TCHAR szUnknown[] = TEXT("<Unknown>");
+TCHAR szNoDebugPriv[] = TEXT("<NoDebugPriv>");
+TCHAR szAccessDenied[] = TEXT("<AccessDenied>");
+TCHAR szX86[] = TEXT("x86");
+TCHAR szX64[] = TEXT("x64");
+
+COLORREF colorGrey = RGB(0xf1, 0xf2, 0xf6);
+COLORREF colorWhite = RGB(0xff, 0xff, 0xff);
---
+++ global.h
@@ -0,0 +1,14 @@
+#pragma once
+#define DEV
+
+#include <windows.h>
+
+extern TCHAR szUnknown[];
+extern TCHAR szNoDebugPriv[];
+extern TCHAR szAccessDenied[];
+extern TCHAR szX86[];
+extern TCHAR szX64[];
+
+
+extern COLORREF colorGrey;
+extern COLORREF colorWhite;
