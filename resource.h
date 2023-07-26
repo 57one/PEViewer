33f0ae4d0c500ab0821ecf3275e01e5c0d085b31
57one <784537055@qq.com>
2023-07-26 18:22:07 +0800

    update mainDialog layout

--- resource.h
+++ resource.h
@@ -3,13 +3,14 @@
 // Used by resource.rc
 //
 #define IDD_DIALOG                      101
-#define IDC_BUTTON_PEViewer             1001
-#define IDC_BUTTON2                     1002
+#define IDC_BUTTON_PEVIEWER             1001
 #define IDC_LIST_PROCESS                1008
-#define IDC_BUTTON_About                1009
+#define IDC_BUTTON_PROFILE              1009
 #define IDC_BUTTON_QUIT                 1010
-#define IDC_LIST3                       1011
 #define IDC_LIST_MODULE                 1011
+#define IDC_GROUP_LIST                  1012
+#define IDC_GROUP_BUTTON                1013
+#define IDC_LIST_PROCESS2               1016
 
 // Next default values for new objects
 // 
@@ -17,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
