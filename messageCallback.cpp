5b89cf3bc8b99fcf6b6b4435a25ef4c6fd6c3529
57one <784537055@qq.com>
2023-08-10 17:49:40 +0800

    add onPeEditor and onPeEditorInit methods

--- messageCallback.cpp
+++ messageCallback.cpp
@@ -1,5 +1,8 @@
 #include "messageCallback.h"
 
+#include <stdio.h>
+
+#include "windowProc.h"
 void onSize(HWND mainDialogHwnd, LPSIZE mainDialogSize, WPARAM wParam,
             LPARAM lParam) {
   switch (wParam) {
@@ -25,7 +28,7 @@ void onSize(HWND mainDialogHwnd, LPSIZE mainDialogSize, WPARAM wParam,
                     TRUE, TRUE);
   hdwp = moveWidget(mainDialogHwnd, hdwp, IDC_GROUP_BUTTON, x, y, TRUE, FALSE,
                     TRUE, TRUE);
-  hdwp = moveWidget(mainDialogHwnd, hdwp, IDC_BUTTON_PEVIEWER, x, y, TRUE,
+  hdwp = moveWidget(mainDialogHwnd, hdwp, IDC_BUTTON_PE_EDITOR, x, y, TRUE,
                     FALSE, TRUE, FALSE);
   hdwp = moveWidget(mainDialogHwnd, hdwp, IDC_BUTTON_PROFILE, x, y, TRUE, TRUE,
                     TRUE, TRUE);
@@ -39,4 +42,45 @@ void onSize(HWND mainDialogHwnd, LPSIZE mainDialogSize, WPARAM wParam,
 void onGetMinMaxInfo(MINMAXINFO* lpMinMaxInfo) {
   lpMinMaxInfo->ptMinTrackSize.x = 630;
   lpMinMaxInfo->ptMinTrackSize.y = 300;
+}
+
+void onPeEditor(HWND hwnd) {
+  OPENFILENAME ofn = {0};
+  TCHAR szBuffer[MAX_PATH * 10] = {0};  // file name
+  HANDLE hf = NULL;                     // file handle
+  ofn.lStructSize = sizeof(OPENFILENAME);
+  ofn.hwndOwner = hwnd;
+  ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_EXPLORER;
+  ofn.lpstrFile = szBuffer;
+  ofn.lpstrFile[0] = '\0';
+  ofn.nMaxFile = sizeof(szBuffer) / sizeof(*szBuffer);
+  ofn.lpstrFilter = TEXT(
+      "Executable "
+      "Files(.exe;.scr;.dll;.ocx;.cpl;.drv)\0*.exe;*.scr;*.dll;*.ocx;*.cpl;*."
+      "drv\0All Files(*.*)\0*.*\0");
+  ofn.lpstrInitialDir = NULL;
+  ofn.nFilterIndex = 0;
+  ofn.lpstrFileTitle = NULL;
+  ofn.nMaxFileTitle = 0;
+
+  // display the open dialog box.
+  if (GetOpenFileName(&ofn) == TRUE) {
+    /*_tprintf(TEXT("%s\n"), szBuffer);*/
+    // Display the Open dialog box.
+    readPeFile(szBuffer, &pFileBuffer);
+    DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_PE_EDITOR),
+                   hwnd, PeEditorProc, (LPARAM)szBuffer);
+  }
+}
+
+void onPeEditorInit(HWND hwnd, LPARAM lParam) {
+  // dynamically change the title of dialogbox
+  TCHAR szTitle[MAX_PATH];
+  GetWindowText(hwnd, szTitle, sizeof(szTitle) / sizeof(*szTitle));
+  _tcscat_s(szTitle, TEXT(" -- "));
+  _tcscat_s(szTitle, (PTCHAR)lParam);
+  SetWindowText(hwnd, szTitle);
+
+  // set text about dos header
+  readDosHeader(hwnd, pFileBuffer);
 }
--- messageCallback.h
+++ messageCallback.h
@@ -6,9 +6,17 @@
 #include "global.h"
 #include "resource.h"
 #include "utility.h"
+#include "readPE.h"
 
 // WM_SIZE
 void onSize(HWND mainDialogHwnd, LPSIZE mainDialogSize, WPARAM wParam,
             LPARAM lParam);
 
+// WM_GETMINMAXINFO
 void onGetMinMaxInfo(MINMAXINFO* lpMMI);
+
+// WM_COMMAND BUTTON_PE_EDITOR
+void onPeEditor(HWND hwnd);
+
+// WM_INITDIALOG
+void onPeEditorInit(HWND hwnd, LPARAM lParam);
