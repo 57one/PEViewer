7987f8750f52069597a0c62b7d2427f0b4ea86c2
57one <784537055@qq.com>
2023-08-10 17:46:47 +0800

    update resource

--- resource.h
+++ resource.h
@@ -3,22 +3,82 @@
 // Used by resource.rc
 //
 #define IDD_DIALOG                      101
-#define IDC_BUTTON_PEVIEWER             1001
+#define IDD_DIALOG_ABOUT                107
+#define IDD_DIALOG_PE_EDITOR            134
+#define IDB_BITMAP1                     141
+#define IDC_BUTTON_PE_EDITOR            1001
 #define IDC_LIST_PROCESS                1008
 #define IDC_BUTTON_PROFILE              1009
+#define IDC_BUTTON_ABOUT                1009
 #define IDC_BUTTON_QUIT                 1010
 #define IDC_LIST_MODULE                 1011
 #define IDC_GROUP_LIST                  1012
 #define IDC_GROUP_BUTTON                1013
-#define IDC_LIST_PROCESS2               1016
+#define IDC_ABOUT_PIC                   1018
+#define IDB_BITMAP_PEVIEWER             1018
+#define IDC_BUTTON_OK                   1019
+#define IDC_STATIC_                     1020
+#define IDC_TEXT_7EVIEWER               1020
+#define IDC_TEXT_INTRO                  1021
+#define IDC_TEXT_CREATED                1022
+#define IDC_TEXT_CREATOR                1023
+#define IDC_GROUP_DOS_HEADER            1024
+#define IDC_GROUP_INFO                  1025
+#define IDC_BUTTON_FILE_HEADER          1026
+#define IDC_BUTTON_OPTION_HEADER        1027
+#define IDC_BUTTON_HEADER_INFO          1028
+#define IDC_BUTTON_SECTION              1029
+#define IDC_GROUP_TOOLS                 1030
+#define IDC_BUTTON_DIRECTORY            1031
+#define IDC_BUTTON_TOOLS                1032
+#define IDC_GROUP_OK                    1033
+#define IDC_BUTTON_EDIT                 1034
+#define IDC_BUTTON_CANCEL               1035
+#define IDC_STATIC_MAGIC_NUMBER         1036
+#define IDC_STATIC_BLG                  1037
+#define IDC_STATIC_BLP                  1037
+#define IDC_EDIT_MAGIC_NUMBER           1038
+#define IDC_STATIC_CHECKSUM             1039
+#define IDC_EDIT_CHECKSUM               1040
+#define IDC_EDIT_BLG                    1041
+#define IDC_EDIT_BLP                    1041
+#define IDC_STATIC_PAGES                1042
+#define IDC_EDIT_PAGES                  1043
+#define IDC_STATIC_RLC                  1044
+#define IDC_EDIT_RLC                    1045
+#define IDC_STATIC_PARHDR               1046
+#define IDC_EDIT_PARHDR                 1047
+#define IDC_STATIC_MIN_ALLOC            1048
+#define IDC_EDIT_MIN_ALLOC              1049
+#define IDC_STATIC_MAX_ALLOC            1050
+#define IDC_EDIT_MAX_ALLOC              1051
+#define IDC_STATIC_INIT_SS              1052
+#define IDC_EDIT_INIT_SS                1053
+#define IDC_STATIC_INIT_SP              1054
+#define IDC_EDIT_INIT_SP                1055
+#define IDC_STATIC_INIT_IP              1056
+#define IDC_EDIT_INIT_IP                1057
+#define IDC_STATIC_INIT_CS              1058
+#define IDC_EDIT_INIT_CS                1059
+#define IDC_STATIC_LFARLC               1060
+#define IDC_EDIT_LFARLC                 1061
+#define IDC_STATIC_OVNO                 1062
+#define IDC_EDIT_OVNO                   1063
+#define IDC_STATIC_OEM_ID               1064
+#define IDC_EDIT_OEM_ID                 1065
+#define IDC_STATIC_OEM_INFO             1066
+#define IDC_EDIT_OEM_INFO               1067
+#define IDC_STATIC_PE_ADDRESS           1068
+#define IDC_EDIT_OEM_INFO2              1069
+#define IDC_EDIT_PE_ADDRESS             1069
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        142
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1018
+#define _APS_NEXT_CONTROL_VALUE         1039
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
