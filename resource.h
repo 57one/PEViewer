6be4ddfe8c8b4e940a4459842417539dd7ea2f2f
57one <784537055@qq.com>
2023-08-17 23:59:52 +0800

    add Image_File_Header layout

--- resource.h
+++ resource.h
@@ -5,7 +5,7 @@
 #define IDD_DIALOG                      101
 #define IDD_DIALOG_ABOUT                107
 #define IDD_DIALOG_PE_EDITOR            134
-#define IDB_BITMAP1                     141
+#define IDD_DIALOG_IMAGE_FILE_HEADER    142
 #define IDC_BUTTON_PE_EDITOR            1001
 #define IDC_LIST_PROCESS                1008
 #define IDC_BUTTON_PROFILE              1009
@@ -33,7 +33,9 @@
 #define IDC_BUTTON_TOOLS                1032
 #define IDC_GROUP_OK                    1033
 #define IDC_BUTTON_EDIT                 1034
+#define IDC_BUTTON_DOS_OK               1034
 #define IDC_BUTTON_CANCEL               1035
+#define IDC_BUTTON_DOS_CANCEL           1035
 #define IDC_STATIC_MAGIC_NUMBER         1036
 #define IDC_STATIC_BLG                  1037
 #define IDC_STATIC_BLP                  1037
@@ -47,25 +49,44 @@
 #define IDC_STATIC_RLC                  1044
 #define IDC_EDIT_RLC                    1045
 #define IDC_STATIC_PARHDR               1046
+#define IDC_STATIC_MACHINE              1046
 #define IDC_EDIT_PARHDR                 1047
+#define IDC_EDIT_MACHINE                1047
 #define IDC_STATIC_MIN_ALLOC            1048
+#define IDC_STATIC_NUMBER_OF_SECTIONS   1048
 #define IDC_EDIT_MIN_ALLOC              1049
+#define IDC_EDIT_NUMBER_OF_SECTIONS     1049
 #define IDC_STATIC_MAX_ALLOC            1050
+#define IDC_STATIC_TIMEDATESTAMP        1050
 #define IDC_EDIT_MAX_ALLOC              1051
+#define IDC_EDIT_TIMEDATESTAMP          1051
 #define IDC_STATIC_INIT_SS              1052
+#define IDC_STATIC_P_SYMTAB             1052
 #define IDC_EDIT_INIT_SS                1053
+#define IDC_EDIT_P_SYMTAB               1053
 #define IDC_STATIC_INIT_SP              1054
+#define IDC_STATIC_SIZE_OF_OPT_HEADER   1054
 #define IDC_EDIT_INIT_SP                1055
+#define IDC_EDIT_SIZE_OF_OPT_HEADER     1055
 #define IDC_STATIC_INIT_IP              1056
+#define IDC_STATIC_CHARACTERISTICS      1056
 #define IDC_EDIT_INIT_IP                1057
+#define IDC_EDIT_CHARACTERISTICS        1057
 #define IDC_STATIC_INIT_CS              1058
 #define IDC_EDIT_INIT_CS                1059
+#define IDC_BUTTON_MACHINE              1059
 #define IDC_STATIC_LFARLC               1060
+#define IDC_STATIC_NUMBER_OF_SYMBOL     1060
 #define IDC_EDIT_LFARLC                 1061
+#define IDC_EDIT_NUMBER_OF_SYMBOL       1061
 #define IDC_STATIC_OVNO                 1062
+#define IDC_BUTTON_TIMEDATESTAMP        1062
 #define IDC_EDIT_OVNO                   1063
+#define IDC_BUTTON_CHARACTERISTICS      1063
 #define IDC_STATIC_OEM_ID               1064
+#define IDC_BUTTON_FILE_HEADER_OK       1064
 #define IDC_EDIT_OEM_ID                 1065
+#define IDC_BUTTON_FILE_HEADER_CANCEL   1065
 #define IDC_STATIC_OEM_INFO             1066
 #define IDC_EDIT_OEM_INFO               1067
 #define IDC_STATIC_PE_ADDRESS           1068
@@ -76,9 +97,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        142
+#define _APS_NEXT_RESOURCE_VALUE        144
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1039
+#define _APS_NEXT_CONTROL_VALUE         1066
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
