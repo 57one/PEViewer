01d8e4e3be9d1c703169e23452b3f079aa3c2bab
57one <784537055@qq.com>
2023-08-22 23:54:18 +0800

    add MachineType-relative data

--- global.cpp
+++ global.cpp
@@ -8,3 +8,81 @@ TCHAR szX64[] = TEXT("x64");
 
 COLORREF colorGrey = RGB(0xf1, 0xf2, 0xf6);
 COLORREF colorWhite = RGB(0xff, 0xff, 0xff);
+
+WORD wMachineTypeLength = 30;
+
+TCHAR szMachineType[][40] = {TEXT("IMAGE_FILE_MACHINE_UNKNOWN"),
+                             TEXT("IMAGE_FILE_MACHINE_ALPHA"),
+                             TEXT("IMAGE_FILE_MACHINE_ALPHA64"),
+                             TEXT("IMAGE_FILE_MACHINE_AM33"),
+                             TEXT("IMAGE_FILE_MACHINE_AMD64"),
+                             TEXT("IMAGE_FILE_MACHINE_ARM"),
+                             TEXT("IMAGE_FILE_MACHINE_ARM64"),
+                             TEXT("IMAGE_FILE_MACHINE_ARMNT"),
+                             TEXT("IMAGE_FILE_MACHINE_AXP64"),
+                             TEXT("IMAGE_FILE_MACHINE_EBC"),
+                             TEXT("IMAGE_FILE_MACHINE_I386"),
+                             TEXT("IMAGE_FILE_MACHINE_IA64"),
+                             TEXT("IMAGE_FILE_MACHINE_LOONGARCH32"),
+                             TEXT("IMAGE_FILE_MACHINE_LOONGARCH64"),
+                             TEXT("IMAGE_FILE_MACHINE_M32R"),
+                             TEXT("IMAGE_FILE_MACHINE_MIPS16"),
+                             TEXT("IMAGE_FILE_MACHINE_MIPSFPU"),
+                             TEXT("IMAGE_FILE_MACHINE_MIPSFPU16"),
+                             TEXT("IMAGE_FILE_MACHINE_POWERPC"),
+                             TEXT("IMAGE_FILE_MACHINE_POWERPCFP"),
+                             TEXT("IMAGE_FILE_MACHINE_R4000"),
+                             TEXT("IMAGE_FILE_MACHINE_RISCV32"),
+                             TEXT("IMAGE_FILE_MACHINE_RISCV64"),
+                             TEXT("IMAGE_FILE_MACHINE_RISCV128"),
+                             TEXT("IMAGE_FILE_MACHINE_SH3"),
+                             TEXT("IMAGE_FILE_MACHINE_SH3DSP"),
+                             TEXT("IMAGE_FILE_MACHINE_SH4"),
+                             TEXT("IMAGE_FILE_MACHINE_SH5"),
+                             TEXT("IMAGE_FILE_MACHINE_THUMB"),
+                             TEXT("IMAGE_FILE_MACHINE_WCEMIPSV2")};
+
+WORD wMachineType[] = {
+    0x0,   0x184,  0x284,  0x1d3,  0x8664, 0x1c0, 0xaa64, 0x1c4, 0x284, 0xebc,
+    0x14c, 0x200,  0x6232, 0x6264, 0x9041, 0x266, 0x366,  0x466, 0x1f0, 0x1f1,
+    0x166, 0x5032, 0x5064, 0x5128, 0x1a2,  0x1a3, 0x1a6,  0x1a8, 0x1c2, 0x169};
+
+std::map<WORD, WORD> machinetypeToIndex{
+    {0x0, 0},    {0x184, 1},   {0x284, 2},   {0x1d3, 3},   {0x8664, 4},
+    {0x1c0, 5},  {0xaa64, 6},  {0x1c4, 7},   {0x284, 8},   {0xebc, 9},
+    {0x14c, 10}, {0x200, 11},  {0x6232, 12}, {0x6264, 13}, {0x9041, 14},
+    {0x266, 15}, {0x366, 16},  {0x466, 17},  {0x1f0, 18},  {0x1f1, 19},
+    {0x166, 20}, {0x5032, 21}, {0x5064, 22}, {0x5128, 23}, {0x1a2, 24},
+    {0x1a3, 25}, {0x1a6, 26},  {0x1a8, 27},  {0x1c2, 28},  {0x169, 29}};
+
+TCHAR szMachineTypeDesc[][40] = {
+    TEXT("UNKNOWN"),
+    TEXT("Alpha AXP, 32-bit address space"),
+    TEXT("Alpha 64, 64-bit address space"),
+    TEXT("Matsushita AM33"),
+    TEXT("x64"),
+    TEXT("ARM little endian"),
+    TEXT("ARM64 little endian"),
+    TEXT("ARM Thumb-2 little endian"),
+    TEXT("AXP 64 (Same as Alpha 64)"),
+    TEXT("EFI byte code"),
+    TEXT("Intel 386"),
+    TEXT("Intel Itanium processor family"),
+    TEXT("LoongArch 32-bit processor family"),
+    TEXT("LoongArch 64-bit processor family"),
+    TEXT("Mitsubishi M32R little endian"),
+    TEXT("MIPS16"),
+    TEXT("MIPS with FPU"),
+    TEXT("MIPS16 with FPU"),
+    TEXT("Power PC little endian"),
+    TEXT("Power PC with floating point support"),
+    TEXT("MIPS little endian"),
+    TEXT("RISC-V 32-bit address space"),
+    TEXT("RISC-V 64-bit address space"),
+    TEXT("RISC-V 128-bit address space"),
+    TEXT("Hitachi SH3"),
+    TEXT("Hitachi SH3 DSP"),
+    TEXT("Hitachi SH4"),
+    TEXT("Hitachi SH5"),
+    TEXT("Thumb"),
+    TEXT("MIPS little-endian WCE v2")};
--- global.h
+++ global.h
@@ -2,6 +2,7 @@
 #define DEV
 
 #include <windows.h>
+#include <map>
 
 extern TCHAR szUnknown[];
 extern TCHAR szNoDebugPriv[];
@@ -9,6 +10,9 @@ extern TCHAR szAccessDenied[];
 extern TCHAR szX86[];
 extern TCHAR szX64[];
 
-
 extern COLORREF colorGrey;
 extern COLORREF colorWhite;
+
+extern TCHAR szMachineTypeDesc[][40];
+extern WORD wMachineTypeLength;
+extern std::map<WORD, WORD> machinetypeToIndex;
